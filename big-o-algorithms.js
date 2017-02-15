f17ec1f37378270b0cd7acfeb153e1ac2d44ef75
Mehri Russo <mehrimova@gmail.com>
2017-02-15 09:46:26 -0700

    initial commit

---
+++ big-o-algorithms.js
@@ -0,0 +1,17 @@
+'use strict';
+// write a function for O(1)
+var pets = ['cat', 'dog', 'rat'];
+function findPets(pets) {
+}
+console.log(pets[1]);
+
+// write a function for O(n)
+var pets = ['cat', 'dog', 'rat'];
+function findPets(pets) {
+  for (var i = 0; i < pets.length; i++) {
+    pets[i] = pets[i]
+  }
+}
+console.log(pets[1]);
+
+// write a function for O(n^2)
