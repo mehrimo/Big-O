8ee8d2a780628efe0ed33fb546e4d271fdfc0c51
Mehri Russo <mehrimova@gmail.com>
2017-02-15 10:02:53 -0700

    added function for (n^2)

--- big-o-algorithms.js
+++ big-o-algorithms.js
@@ -15,3 +15,24 @@ function findPets(pets) {
 console.log(pets[1]);
 
 // write a function for O(n^2)
+function findLargestArr(arr) {
+  let total = 0;
+  let bigArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    let sum = 0;
+
+    for (var j = 0; j < arr.length; j++) {
+      sum +=arr[i][j];
+
+      if (sum >= total) {
+        total = sum;
+        bigArr = arr[i]
+      }
+    }
+  }
+  console.log(bigArr, 'is largest array');
+  console.log(total, 'is the total of the largest array');
+}
+
+findLargestArr([[200],[1,10]]);
